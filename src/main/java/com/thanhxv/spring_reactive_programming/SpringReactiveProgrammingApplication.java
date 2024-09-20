4235eb2a5f1cfecab111de462d95f2163abd2e86
thanhdt98 <thanhxv14.doaz@gmail.com>
2024-09-21 02:24:47 +0700

    init project

---
+++ src/main/java/com/thanhxv/spring_reactive_programming/SpringReactiveProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.thanhxv.spring_reactive_programming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanhxv/spring_reactive_programming/SpringReactiveProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhxv.spring_reactive_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
